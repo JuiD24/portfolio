f614a0b17b571a775d7c136b970ff88adfd7c7a2
juid24 <juid24@gmail.com>
2024-06-24 17:28:35 -0400

    added particles and mobile nav back

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Particle from './Particle'
+import Particle from './Particle'
 import TextEffect from './TextEffect'
 import Image from 'next/image'
 import {ArrowDownTrayIcon} from "@heroicons/react/20/solid";
@@ -7,7 +7,7 @@ import {ArrowDownTrayIcon} from "@heroicons/react/20/solid";
 const Hero = () => {
   return (
     <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
-        {/* <Particle/> */}
+        <Particle/>
         <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
             <div>
             <h1 className='text-[35px] md:text-[50px] text-white font-bold'>HI, I AM <span className='text-yellow-400'>JUI!</span></h1>
