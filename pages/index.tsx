c05c2059c19f2883bea075ac7e55e20ab4cce428
juid24 <juid24@gmail.com>
2024-06-24 17:11:35 -0400

    renamed to components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import Contact from "@/temp_components/Contact";
-import Hero from "@/temp_components/Hero";
-import MobileNav from "@/temp_components/MobileNav";
-import Nav from "@/temp_components/Nav";
-import Projects from "@/temp_components/Projects";
-import Skills from "@/temp_components/Skills";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
+import MobileNav from "@/components/MobileNav";
+import Nav from "@/components/Nav";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
-import Education from "@/temp_components/Education";
+import Education from "@/components/Education";
 
 const HomePage = () => {
   const [nav, setNav] = useState(false);
